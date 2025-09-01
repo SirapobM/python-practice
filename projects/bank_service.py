de734707dfb5aaf595062f6b6a30a6d575cc147b
Sirapob <Sirapob.1109@gmail.com>
2025-09-01 20:59:35 +0700

    Refactor bank service code: remove redundant if-elif implementation and update match-case comments

--- projects/bank_service.py
+++ projects/bank_service.py
@@ -1,4 +1,4 @@
-# บริการธนาคาร แบบ match-case (ต้องใช้ Python 3.10+)
+# บริการธนาคาร แบบ match-case 
 user = input("Username : ")
 password = input("Password : ")
 
@@ -14,5 +14,22 @@ if user == "member" and password == "1234":
             print("ยอดเงินคงเหลือ")
         case _:
             print("หมายเลขบริการไม่ถูกต้อง")
+else:
+    print("ไม่พบบัญชี")
+
+# ---------------------------------------------------------------
+# บริการธนาคารแบบ if-elif
+user = input("Username : ")
+password = input("Password : ")
+
+if user == "member" and password == "1234":
+    print("เข้าสู่ระบบสำเร็จ")
+    num = int(input("เลข 1 ถอนเงิน, เลข 2 ฝากเงิน : "))
+    if num == 1:
+        print("ถอนเงิน")
+    elif num == 2:
+        print("ฝากเงิน")
+    else:
+        print("หมายเลขบริการไม่ถูกต้อง")
 else:
     print("ไม่พบบัญชี")
