1673ab46f1c5fb13319962e8fc4e82ca0ef701e5
Sirapob <Sirapob.1109@gmail.com>
2025-09-02 16:37:10 +0700

    Add multiplication functionality for a range of numbers

---
+++ projects/more.py
@@ -0,0 +1,23 @@
+# num = int(input("ใส่เลขแม่สูตรคูณ : "))
+
+# def multiplication(number):
+#     for i in range(1, 13):
+#         print(f"{number} x {i} = {number*i}")
+#     return number   
+
+# multiplication(num)
+
+num_start = int(input("ใส่เลขแม่สูตรคูณเริ่มต้น : "))
+num_end = int(input("ใส่ใส่เลขแม่สูตรคูณจบ : "))
+
+# def multi(number_s, number_e):
+#     for i in range(number_s, number_e + 1):
+#         print(f"{number_s} x {i} = {number_s*i}")
+#     return number_s
+
+def multix(number_s, number_e):
+    for i in range(number_s, number_e + 1):
+        for j in range(1, 13):
+            print(f"{i} x {j} = {i*j}")
+
+multix(num_start, num_end)
