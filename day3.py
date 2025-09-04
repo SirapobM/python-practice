c07d94a21a19b8a0f757a4f3fd8cc0daa1565bf0
Sirapob <Sirapob.1109@gmail.com>
2025-09-04 18:47:52 +0700

    Add saveEmploter function to demonstrate kwargs usage and clean up commented code

--- day3.py
+++ day3.py
@@ -4,3 +4,11 @@
 # time = "ตอนเช้า"
 
 # sayHello(time, 14)
+
+#args / kwargs
+# ข้อมูลแบบลำดับ *args 
+# ข้อมูลแบบกำหนดชื่อ *kwargs
+def saveEmploter(**kwargs):
+    print(f"ชื่อ : {kwargs["name"]} อายุ: {kwargs["age"]} ที่อยู่: {kwargs["address"]}")
+
+saveEmploter(name="ไม้", age=21, address="ไทย")
