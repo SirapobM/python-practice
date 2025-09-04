9811e09fc7c84668f352d083b26e98962bd98a9c
Sirapob <Sirapob.1109@gmail.com>
2025-09-04 19:01:18 +0700

    para + return

--- day3.py
+++ day3.py
@@ -8,7 +8,32 @@
 #args / kwargs
 # ข้อมูลแบบลำดับ *args 
 # ข้อมูลแบบกำหนดชื่อ *kwargs
-def saveEmploter(**kwargs):
-    print(f"ชื่อ : {kwargs["name"]} อายุ: {kwargs["age"]} ที่อยู่: {kwargs["address"]}")
+# def saveEmploter(**kwargs):
+#     print(f"ชื่อ : {kwargs["name"]} อายุ: {kwargs["age"]} ที่อยู่: {kwargs["address"]}")
 
-saveEmploter(name="ไม้", age=21, address="ไทย")
+# saveEmploter(name="ไม้", age=21, address="ไทย")
+
+#return function
+# def get():
+#     return 2
+
+# data = get()
+# print(f"num : {data}")
+
+# para + return
+# def checknum(number):
+#     if number%2==0:
+#         return "เลขคู่"
+#     else: 
+#         return "เลขคี่"
+    
+# result = checknum(3)
+# print(result)
+
+def sum(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total
+
+print(sum(1,2,3))
